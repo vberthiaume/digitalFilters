95aabfdc25188bd7307b76addab2afbbed20b049
Vincent Berthiaume <vberthiaume@gmail.com>
2016-07-01 16:49:43 -0400

    refactor

--- fig2.2.py
+++ fig2.2.py
@@ -6,26 +6,16 @@ import scipy.signal
 N = 10 			# length of test input signal
 x = np.arange(1,N)	# test input signal
 B = [1.0, 1.0]  	# feedforward coefficients
-A = [1.0]      		# feedback coefficients (no-feedback case)
+A = 1.0      		# feedback coefficients (no-feedback case)
 
 
 # PROCESS IT ALL IN ONE SHOT
 y = scipy.signal.lfilter(B, A, x)       
 print "all in one shot:", y
 
-
-
-
 # PROCESS IT IN BATCHES
-NB = N/2		
-y1, Sf  = scipy.signal.lfilter(B, A, x[0:NB], zi = [0])       
-y2, Sf2 = scipy.signal.lfilter(B, A, x[NB:N], zi = Sf)  
+NB = N/2
+# in matlab, indices start at 1, but in python they start at 0
+y1, zf  = scipy.signal.lfilter(B, A, x[0:NB], zi = [0])	#we need to have a zi to get a zf
+y2, zf2 = scipy.signal.lfilter(B, A, x[NB:N], zi = zf)	#we need to store the Zf2 somewhere, otherwise it just goes into y2  
 print "in 2 shots:\t", y1, y2
-
-
-
-#   disp(sprintf('x(%d)=%f\ty(%d)=%f',i,x(i),i,y2(i-NB)));
-
-
-
-
