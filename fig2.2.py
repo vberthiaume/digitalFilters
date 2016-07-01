4d86f7829c1a50781cd8920a8b5e7d257d953bd4
Vincent Berthiaume <vberthiaume@gmail.com>
2016-07-01 15:24:34 -0400

    lfilter running as non-block

--- fig2.2.py
+++ fig2.2.py
@@ -3,23 +3,29 @@
 import numpy as np
 import scipy.signal
 
-
 N = 10 			# length of test input signal
 NB = N/2		# block length
 x = np.arange(1,N)	# test input signal
-B = [1, 1]  		# feedforward coefficients
-A = [1]      		# feedback coefficients (no-feedback case)
+B = [1.0, 1.0]  	# feedforward coefficients
+A = [1.0]      		# feedback coefficients (no-feedback case)
 
 # process block 1
-[y1, Sf] = scipy.signal.lfilter(B, A, x[1:NB])       
+#[y1, Sf] = scipy.signal.lfilter(B, A, x[1:NB])       
+y1 = scipy.signal.lfilter(B, A, x)       
+
+print y1
+
+#the problem with stuff below is that i don't understand how to use lfilter in a block way. 
+#apparently this is possible, see here:
+#http://stackoverflow.com/questions/8922657/matlab-filter-with-scipy-lfilter
+
 
 # process block 2
-y2 = scipy.signal.lfilter(B, A, x[NB+1:N], Sf)  
+#y2 = scipy.signal.lfilter(B, A, x[NB+1:N], Sf)  
 
 # print input and output for block 1
 #for i in range (1, NB):   
 #  disp(sprintf('x(%d)=%f\ty(%d)=%f',i,x(i),i,y1(i)));
-
 
 # print input and output for block 2
 #for i in range (NB+1, N):
