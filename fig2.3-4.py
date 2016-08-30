9a48f3842f89cc2c48da289be3e512d5ccc010b2
Vincent Berthiaume <vberthiaume@gmail.com>
2016-08-29 20:16:27 -0400

    fix spacing

--- fig2.3-4.py
+++ fig2.3-4.py
@@ -1,36 +1,36 @@
 def swanal(t,f,B,A):
 
-	ampin   = 1    			# input signal amplitude
-	phasein = 0    			# input signal phase
-	N 		= length(f)       	# number of test frequencies
-	gains 	= zeros(1,N)  		# pre-allocate amp-response array
-	phases 	= zeros(1,N) 		# pre-allocate phase-response array
+    ampin   = 1                 # input signal amplitude
+    phasein = 0                 # input signal phase
+    N       = length(f)         # number of test frequencies
+    gains   = zeros(1,N)        # pre-allocate amp-response array
+    phases  = zeros(1,N)        # pre-allocate phase-response array
 
-	if length(A)==1: 
-	 	ntransient=length(B)-1 	# no. samples to steady state
-	else : 
-		error('Need to set transient response duration here')
+    if length(A)==1: 
+        ntransient=length(B)-1  # no. samples to steady state
+    else : 
+        error('Need to set transient response duration here')
 
-	for k in range (length(f)):    				# loop over analysis frequencies
-		s = ampin*cos(2*pi*f(k)*t+phasein) 	# test sinusoid
-	 	y = filter(B,A,s) 						# run it through the filter
-	 	yss = y[ntransient:length(y)] 		# chop off transient
-	 	
-	 	# measure output amplitude as max (SHOULD INTERPOLATE):
-	 	[ampout,peakloc] = max(abs(yss)) 		# ampl. peak & index
-	 	gains(k) = ampout/ampin  				# amplitude response
-	 
-	 	if ampout < eps:  		# eps returns "machine epsilon"
-	   		phaseout=0    		# phase is arbitrary at zero gain
-	 	else:
-	   		sphase = 2*pi*f(k)*(peakloc+ntransient-1)
+    for k in range (length(f)):             # loop over analysis frequencies
+        s = ampin*cos(2*pi*f(k)*t+phasein)  # test sinusoid
+        y = filter(B,A,s)                   # run it through the filter
+        yss = y[ntransient:length(y)]       # chop off transient
+        
+        # measure output amplitude as max (SHOULD INTERPOLATE):
+        [ampout,peakloc] = max(abs(yss))        # ampl. peak & index
+        gains(k) = ampout/ampin                 # amplitude response
+     
+        if ampout < eps:        # eps returns "machine epsilon"
+            phaseout=0          # phase is arbitrary at zero gain
+        else:
+            sphase = 2*pi*f(k)*(peakloc+ntransient-1)
 
-	   		# compute phase by inverting sinusoid (BAD METHOD):
-	   		phaseout = acos(yss(peakloc)/ampout) - sphase
-	   		phaseout = mod2pi(phaseout) 	# reduce to [-pi,pi)
+            # compute phase by inverting sinusoid (BAD METHOD):
+            phaseout = acos(yss(peakloc)/ampout) - sphase
+            phaseout = mod2pi(phaseout)     # reduce to [-pi,pi)
 
-	 
-	 	phases(k) = phaseout-phasein
-	 	swanalplot # signal plotting script
+     
+        phases(k) = phaseout-phasein
+        swanalplot # signal plotting script
 
-	return gains, phases
+    return gains, phases
