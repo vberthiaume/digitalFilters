30331b792b1b111ca657a9d6162fbb93943c40ff
vberthiaume <vberthiaume@gmail.com>
2016-09-13 12:49:58 -0400

    tried as best as I could, there is a problem with the calculation of the phase for figure 2.7, line 55 of fig2.3-4.py

--- fig2.3-4.py
+++ fig2.3-4.py
@@ -14,11 +14,11 @@ import swanalmainplot as smain
 def mod2pi(x):
     # MOD2PI - Reduce x to the range [-pi,pi)
     y = x
-    twopi = 2 * np.pi
+    twopi = 2.0 * np.pi
     while y >= np.pi:
-        y = y - twopi
+        y -= twopi
     while y < -np.pi:
-        y = y + twopi
+        y += twopi
     return y
 
 ################################ FIG 2.4 P.125 ################################
@@ -26,34 +26,33 @@ def swanal(t,f,B,A):
 
     ampin   = 1                                 # input signal amplitude
     phasein = 0                                 # input signal phase
-    N       = len(f)                         # number of test frequencies
-    gains   = np.zeros(N)                        # pre-allocate amp-response array
-    phases  = np.zeros(N)                        # pre-allocate phase-response array
+    N       = len(f)                            # number of test frequencies
+    gains   = np.zeros(N)                       # pre-allocate amp-response array
+    phases  = np.zeros(N)                       # pre-allocate phase-response array
 
     if len(A)==1 : 
-        ntransient = len(B)-1                # no. samples to steady state
+        ntransient = len(B)-1                   # no. samples to steady state
     else : 
         error('Need to set transient response duration here')
 
     for k in range (len(f)):                            # loop over analysis frequencies
         s   = ampin*np.cos(2*np.pi*f[k]*t+phasein)      # test sinusoid
         y   = scipy.signal.lfilter(B,A,s)               # run it through the filter
-        yss = y[ntransient:len(y)]                      # chop off transient
+        yss = y[ntransient:len(y)+1]                      # chop off transient
         
         # measure output amplitude as max (SHOULD INTERPOLATE):
-        #[ampout, peakloc] = np.max(abs(yss))           # ampl. peak & index
-        yss = abs(yss)
-        ampout, peakloc = np.max(yss), np.argmax(yss)   # ampl. peak & index	
+        yss     = np.abs(yss)
+        ampout  = np.max(yss)                           # peak amplitude
+        peakloc = np.argmax(yss)                        # index of the peak amplitude	
 
         gains[k] = ampout/ampin                         # amplitude response
      
         if ampout < sys.float_info.epsilon:             # eps returns "machine epsilon"
             phaseout = 0                                # phase is arbitrary at zero gain
         else:
-            sphase = 2*np.pi * f[k] * (peakloc+ntransient-1)
-
             # compute phase by inverting sinusoid (BAD METHOD):
-            phaseout = np.arccos(yss[peakloc]/ampout) - sphase
+            #THERE IS SOMETHING WRONG WITH THIS CALCULATION, I CAN'T FIGURE IT OUT
+            phaseout = np.arccos(yss[peakloc]/ampout) - 2*np.pi * f[k] * (peakloc+ntransient-1)
             phaseout = mod2pi(phaseout)                 # reduce to [-pi,pi)
 
         phases[k] = phaseout-phasein
@@ -73,10 +72,10 @@ N               = 10.0                          # number of sinusoidal test freq
 fs              = 1.0                           # sampling rate in Hz (arbitrary)
 fmax            = fs/2                          # highest frequency to look at
 df              = fmax/(N-1)                    # spacing between frequencies
-f               = np.arange(0, fmax+df/2, df)        # sampled frequency axis
+f               = np.arange(0, fmax+df/2, df)   # sampled frequency axis
 dt              = 1/fs                          # sampling interval in seconds
 tmax            = 10.0                          # number of seconds to run each sine test
-t               = np.arange(0, tmax+dt/2, dt)        # sampled time axis
+t               = np.arange(0, tmax+dt/2, dt)   # sampled time axis
 
 #ACTUAL ANALYSES
 [gains, phases]  = swanal(t,f/fs,B,A)           # sine-wave analysis
