cf2397beccfd36e7158994967e481a1ea29a4946
vberthiaume <vberthiaume@gmail.com>
2016-09-24 13:58:07 -0400

    new file

---
+++ ch3/p152fig3.9.py
@@ -0,0 +1,93 @@
+import numpy as np
+import scipy.signal
+import matplotlib.pyplot as plt
+
+#def filterslow(B, A, x):
+#    # FILTERSLOW: Filter x to produce y = (B/A) x . Equivalent to 'y = filter(B,A,x)' using a slow (but tutorial) method.
+#    NB = len(B)
+#    NA = len(A)
+#    Nx = len(x)
+#    xv = x  #x(:)          # ensure column vector
+
+#    # do the FIR part using vector processing, store it in v
+#    v = B[0]*xv
+#    if NB > 1:
+#        for i in np.arange(1, min(NB,Nx)):
+#            xdelayed = np.hstack((np.zeros(i-1), xv[0:Nx-i+1]))
+#            v = v + B[i] * xdelayed
+    
+#    # The feedback part is intrinsically scalar, so this loop is where we spend a lot of time.
+#    y  = np.zeros(len(x))               # pre-allocate y
+#    ac = -A[1:NA]                       # could be NA +1
+#    for i in range (Nx):                # loop over input samples
+#        t = v[i]                        # initialize accumulator
+#        if NA > 1:
+#            for j in range (NA-1):
+#                if i > j:
+#                    t += ac[j]*y[i-j]
+#                #else y(i-j) = 0
+#        y[i] = t
+#    #y = reshape(y, len(x)) # in case x was a row vector
+#    return y
+
+
+#N = 10000
+#x  = np.random.random(N)                            # random input signal
+#B  = np.random.random(101)                          # random coefficients
+#A  = np.hstack((1, 0.001*np.random.random(100)))    # random but probably stable
+
+##tic 
+#yf = scipy.signal.lfilter(B, A, x)
+##ft = toc
+##tic 
+#yfs = filterslow(B, A, x)
+##fst=toc
+
+
+## my plottings
+#fig, axarr = plt.subplots(3, sharex=False)
+
+#axarr[0].plot(np.arange(N), x)
+#axarr[0].set_title('random numbers')
+    
+#axarr[1].plot(np.arange(N), yf)
+#axarr[1].set_title('lfiltered random numbers')
+
+#axarr[2].plot(np.arange(N), yfs)
+#axarr[2].set_title('filtered slow random numbers')
+
+#plt.show()
+
+
+
+# Example comb filter:
+
+# Feedforward coeffs
+g1 = 0.5 ** 3 
+B = [1, 0, 0, g1]              
+
+# Feedback coefficients
+g2 = 0.9 ** 5 
+A = [1, 0, 0, 0, 0, g2]          
+h = scipy.signal.lfilter(B, A, np.hstack((1, np.zeros(50))))    # Impulse response
+
+fig, axarr = plt.subplots(1, sharex=False)
+
+axarr[0].set_title('impulse response')
+axarr[0].stem(np.arange(50), y)
+
+
+# Matlab-compatible plot:
+#clf 
+#figure(1) 
+#stem([0:50],h,'-k') 
+#axis([0 50 -0.8 1.1])
+#ylabel('Amplitude') 
+#xlabel('Time (samples)') 
+#grid
+#cmd = 'print -deps ../eps/eir.eps' 
+#disp(cmd) 
+#eval(cmd)
+
+
+
