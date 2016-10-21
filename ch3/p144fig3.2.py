bca18c39b16d46c00ebbd30e52bcd3da59bf7b53
vberthiaume <vberthiaume@gmail.com>
2016-10-21 14:29:52 -0400

    refactor

--- ch3/p144fig3.2.py
+++ ch3/p144fig3.2.py
@@ -14,27 +14,34 @@ def filterslow(B, A, x):
     #other indices in B contain gain for delayed input, ie, feedforward gains
     if NB > 1:
         # we loop until we run out of either feedforward gains or samples
-        for i in np.arange(1, min(NB, Nx)):
+        for k in np.arange(1, min(NB, Nx)):
             # to delay the input by i-1 samples, we simply insert i-1 0s in front of x 
-            padding  = np.zeros(i - 1)
-            xdelayed = np.hstack((padding, x[0 : Nx-i+1]))
+            padding  = np.zeros(k - 1)
+            xdelayed = np.hstack((padding, x[0 : Nx-k+1]))
             #having 0s at the front of xdelayed means that the corresponding samples in v will not be updated
             #because they were already processed on previous iterations
-            v = v + B[i] * xdelayed
+            v += B[k] * xdelayed
     
     # The feedback part is intrinsically scalar, so this loop is where we spend a lot of time.
-    y  = np.zeros(len(x))               # pre-allocate y
-    ac = -A[1:NA]                       # could be NA +1
-    for i in range (Nx):                # loop over input samples
+    y  = np.zeros(len(x))              
+    ac = A[1:NA]                        #so we just ignore A[0]?    
+    
+    # loop over input samples
+    for i in range (Nx):                
         t = v[i]                        # initialize accumulator
+        #if we have feedback gains
         if NA > 1:
-            for j in range (NA-1):
-                if i > j:
-                    t += ac[j]*y[i-j]
-                #else y(i-j) = 0
+            for k in range (NA-1):
+                if i > k:
+                    t -= ac[k]*y[i-k]
+                #else y(i-k) = 0
         y[i] = t
-    #y = reshape(y, len(x)) # in case x was a row vector
+
     return y
+
+
+
+
 
 
 N = 10000
