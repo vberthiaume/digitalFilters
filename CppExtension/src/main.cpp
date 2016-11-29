90dc594ad99fe4774015305f2af801e6ac31d3c6
vberthiaume <vberthiaume@gmail.com>
2016-11-29 15:03:09 -0500

    extension now building when using python 3.5

--- CppExtension/src/main.cpp
+++ CppExtension/src/main.cpp
@@ -1,19 +1,32 @@
 #include "include.h"
 
-PyMethodDef SpamMethods[] = {
-	{0,0,0,0}
-};
 
-PyMODINIT_FUNC
-initspam(void)
-{
-    PyObject *m;
+//CODE FROM 2.7 TUTORIAL
+//PyMethodDef SpamMethods[] = {
+//	{0,0,0,0}
+//};
+//
+//PyMODINIT_FUNC
+//initspam(void)
+//{
+//    PyObject *m;
+//
+//    m = Py_InitModule("spam", SpamMethods);
+//    if (m == NULL)
+//        return;
+//
+//    //SpamError = PyErr_NewException("spam.error", NULL, NULL);
+//    //Py_INCREF(SpamError);
+//    //PyModule_AddObject(m, "error", SpamError);
+//}
 
-    m = Py_InitModule("spam", SpamMethods);
-    if (m == NULL)
-        return;
+//CODE FROM 3.5 TUTORIAL
+static PyObject *spam_system(PyObject *self, PyObject *args) {
+    const char *command;
 
-    //SpamError = PyErr_NewException("spam.error", NULL, NULL);
-    //Py_INCREF(SpamError);
-    //PyModule_AddObject(m, "error", SpamError);
+    if (!PyArg_ParseTuple(args, "s", &command)){
+        return NULL;
+	}
+    int sts = system(command);
+    return PyLong_FromLong(sts);
 }
