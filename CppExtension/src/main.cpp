9d4f3ff9283e75ef56a0f0af66fd4d6d83219551
vberthiaume <vberthiaume@gmail.com>
2016-11-29 11:26:46 -0500

    Revert "remove cpp extension"
    This reverts commit d7dcd8c9fc2d7301c65f3ae9788a7446b6974342.

---
+++ CppExtension/src/main.cpp
@@ -0,0 +1,19 @@
+#include "include.h"
+
+PyMethodDef SpamMethods[] = {
+	{0,0,0,0}
+};
+
+PyMODINIT_FUNC
+initspam(void)
+{
+    PyObject *m;
+
+    m = Py_InitModule("spam", SpamMethods);
+    if (m == NULL)
+        return;
+
+    //SpamError = PyErr_NewException("spam.error", NULL, NULL);
+    //Py_INCREF(SpamError);
+    //PyModule_AddObject(m, "error", SpamError);
+}
