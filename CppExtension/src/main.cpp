a0281ac42128f3422e534d9b2ef662b9def75b4c
vberthiaume <vberthiaume@gmail.com>
2016-12-06 10:09:26 -0500

    removed commented code from 2.7 tutorial

--- CppExtension/src/main.cpp
+++ CppExtension/src/main.cpp
@@ -1,36 +1,6 @@
 #include "include.h"
 
 
-//CODE FROM 2.7 TUTORIAL
-//PyMethodDef SpamMethods[] = {
-//	{0,0,0,0}
-//};
-//
-//PyMODINIT_FUNC
-//initspam(void)
-//{
-//    PyObject *m;
-//
-//    m = Py_InitModule("spam", SpamMethods);
-//    if (m == NULL)
-//        return;
-//
-//    //SpamError = PyErr_NewException("spam.error", NULL, NULL);
-//    //Py_INCREF(SpamError);
-//    //PyModule_AddObject(m, "error", SpamError);
-//}
-
-//CODE FROM 3.5 TUTORIAL
-//static PyObject *spam_system(PyObject *self, PyObject *args) {
-//    const char *command;
-//
-//    if (!PyArg_ParseTuple(args, "s", &command)){
-//        return NULL;
-//	}
-//    int sts = system(command);
-//    return PyLong_FromLong(sts);
-//}
-
 PyMethodDef VbCppMethods[] = {
 	{0,0,0,0}
 };
